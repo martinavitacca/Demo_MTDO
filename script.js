fc0ff88b9de7d99059d1ab91eddcb68b8380b21d
martinavitacca <muni_1792@hotmail.com>
2023-05-22 01:25:49 -0300

    Update 8

--- script.js
+++ script.js
@@ -1,46 +1,38 @@
 
 // Funciones para el overlay
 function openNav() {
-    document.getElementById("menu").style.width = "100%";
-    document.getElementById("openMenu").hidden = true;
-    document.getElementById("closeMenu").style.visibility = "visible";
+  document.getElementById("menu").style.width = "100%";
+  document.getElementById("openMenu").hidden = true;
+  document.getElementById("closeMenu").style.visibility = "visible";
 }
   
 function closeNav() {
-    document.getElementById("menu").style.width = "0%";
-    document.getElementById("openMenu").hidden = false;
-    document.getElementById("closeMenu").style.visibility = "hidden";
+  document.getElementById("menu").style.width = "0%";
+  document.getElementById("openMenu").hidden = false;
+  document.getElementById("closeMenu").style.visibility = "hidden";
 }
-// 
 
 
-
-//Get the button
+// Get the button
 let mybutton = document.getElementById("btn-back-to-top");
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function () {
-scrollFunction();
-};
+window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-if (
-document.body.scrollTop > 20 ||
-document.documentElement.scrollTop > 20
-) {
-mybutton.style.display = "block";
-} else {
-mybutton.style.display = "none";
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
 }
-}
+
 // When the user clicks on the button, scroll to the top of the document
-mybutton.addEventListener("click", backToTop);
-
-function backToTop() {
-document.body.scrollTop = 0;
-document.documentElement.scrollTop = 0;
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
 
-
-
-
+mybutton.addEventListener("click",()=>{
+    topFunction();
+});
