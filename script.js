bc6608a061fb59b4d8c45b4073c51ced38b25ce0
martinavitacca <muni_1792@hotmail.com>
2023-06-11 16:28:37 -0300

    Update 10

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 
 // Funciones para el overlay
 function openNav() {
-  document.getElementById("menu").style.width = "100%";
+  document.getElementById("menu").style.width = "100%"; 
   document.getElementById("openMenu").hidden = true;
   document.getElementById("closeMenu").style.visibility = "visible";
+
 }
   
 function closeNav() {
@@ -16,21 +17,21 @@ function closeNav() {
 // Get the button
 let mybutton = document.getElementById("btn-back-to-top");
 
-// When the user scrolls down 20px from the top of the document, show the button
+// When the user scrolls down 20px from the top of the overlay, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+  if (document.getElementById("scrollUp").scrollTop > 20) {
     mybutton.style.display = "block";
-  } else {
+  } 
+  else {
     mybutton.style.display = "none";
   }
 }
 
-// When the user clicks on the button, scroll to the top of the document
+// When the user clicks on the button, scrolls to the top of the overlay with smoothness
 function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
+  document.getElementById("scrollUp").scrollTo({top: 0, behavior: 'smooth'});
 }
 
 mybutton.addEventListener("click",()=>{
